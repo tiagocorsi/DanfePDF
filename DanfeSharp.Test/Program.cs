c93f3ec77e8db62b78ec14d027da54f5484cc63b
Tiago Henrique Corsi <tiagocorsi@gmail.com>
2023-12-16 12:13:31 -0300

    first commit

---
+++ DanfeSharp.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DanfePDF.Esquemas.NFe;
+using DanfePDF.Modelo;
+using DanfePDF;
+
+namespace DanfePDF.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelo = DanfeViewModelCreator.CriarDeArquivoXml(@"C:\Users\tiago\OneDrive\Área de Trabalho\NFe35231228950650000117550010000011021574310182.XML");
+
+            using (var danfe = new Danfe(modelo))
+            {
+                danfe.Gerar();
+                danfe.Salvar(@"C:\Users\tiago\OneDrive\Área de Trabalho\NFe35231228950650000117550010000011021574310182.PDF");
+            }
+        }
+    }
+}
